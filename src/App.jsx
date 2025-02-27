26ace06802e4b38b176faad0ff57860f64e9fba0
Creation20 <triggerstorm100@gmail.com>
2025-02-27 20:02:03 +0000

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
-import LogoSection from './components/LogoSection/LogoSection'
+import LogoSection from './components/LogoSection/LogoSection' 
 import AboutUs from './components/AboutUs/aboutUs'
 import Milestone from './components/Milestone/Milestone'  
 import HowItWorks from './components/HowItWorks/HowItWorks' 
+import Exclusive from './components/ExclusivePackages/ExclusivePackages'
+import Growth from './components/Growth/Growth'
+import FAQ from './components/FAQ/FAQ'
+import Referral from './components/ReferralPartner/ReferralPartner'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -18,6 +23,11 @@ function App() {
       <AboutUs />
       <Milestone />
       <HowItWorks />
+      <Exclusive />
+      <Growth />
+      <FAQ />
+      <Referral />
+      <Footer />
     </>
   )
 }
